8be8242395e7dd105ecb396e35070f6a3d6b723d
Myeisha Swain <zse0296@gmail.com>
2022-12-21 12:29:48 -0500

    inital resolvers and typeDef set up

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,46 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+
+
+
+const resolvers = {
+    Query: {
+
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+        }
+
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+
+            const user = await User.create(args);
+            return user;
+
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return user;
+        }
+    }
+
+}
+
+module.exports = resolvers;
